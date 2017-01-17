5b3f2daa08b810341b4a06b3cbe45c987289c782
miretz <miretz@gmail.com>
2017-01-17 21:02:15 +0100

    Converting Blueprint to C++
    Former-commit-id: 0e8241a6847de8df4e29bd88d8bb44b0c677dce2 [formerly 00c56346bbc84677d9d5ffae4516672f8f339881]
    Former-commit-id: bd3b605bba53d3f71540b3821937447a47e29f5c

--- Source/S05_TestingGrounds/Public/ChooseNextWaypoint.h
+++ Source/S05_TestingGrounds/Public/ChooseNextWaypoint.h
@@ -16,7 +16,12 @@ class S05_TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
 protected:
+
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
 	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
+
 
 };
