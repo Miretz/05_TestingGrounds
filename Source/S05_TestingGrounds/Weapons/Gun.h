40d61391c3e75c9bf15c1a81c86c5b9a7ae0beff
miretz <miretz@gmail.com>
2017-04-02 15:51:06 +0200

    Reattaching the gun

--- Source/S05_TestingGrounds/Weapons/Gun.h
+++ Source/S05_TestingGrounds/Weapons/Gun.h
@@ -42,13 +42,15 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
 	class USoundBase* FireSound;
 
-	/** AnimMontage to play each time we fire */
+	/** AnimMontages to play each time we fire */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
-	class UAnimMontage* FireAnimation;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
-	class UAnimInstance* AnimInstance;
+	class UAnimMontage* FireAnimation1P;
 
-	
-	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class UAnimMontage* FireAnimation3P;
+
+	class UAnimInstance* AnimInstance1P;
+	class UAnimInstance* AnimInstance3P;
+
+
 };
