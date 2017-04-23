d856878b240c683de4f03825e76bf89b91c849cf
miretz <miretz@gmail.com>
2017-04-23 17:50:23 +0200

    Flyweight pattern for Foliage

--- Source/S05_TestingGrounds/Terrain/Tile.h
+++ Source/S05_TestingGrounds/Terrain/Tile.h
@@ -15,7 +15,7 @@ public:
 	ATile();
 
 	UFUNCTION(BlueprintCallable, Category = "Setup")
-	void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn);
+	void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn = 1, int MaxSpawn = 1, float Radius = 500, float MinScale = 1, float MaxScale = 1);
 
 protected:
 	// Called when the game starts or when spawned
@@ -27,7 +27,11 @@ public:
 
 private:
 
-	bool CastSphere(FVector Location, float Radius);
+	bool FindEmptyLocation(FVector& OutLocation, float Radius);
+
+	void PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint, float Rotation, float Scale);
+
+	bool CanSpawnAtLocation(FVector Location, float Radius);
 
 	
 	
